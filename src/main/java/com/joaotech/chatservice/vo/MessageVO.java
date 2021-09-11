fd2cfff4bfd610eb869abb9700f1f3eaca1bde37
João Gabriel Esteves <jgs.esteves@gmail.com>
2021-09-11 15:29:31 -0300

    Add lombok annotations

--- src/main/java/com/joaotech/chatservice/vo/MessageVO.java
+++ src/main/java/com/joaotech/chatservice/vo/MessageVO.java
@@ -2,11 +2,15 @@ package com.joaotech.chatservice.vo;
 
 import com.joaotech.chatservice.model.MessageStatus;
 import com.joaotech.chatservice.model.MessageType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageVO {
 
     public String token;
