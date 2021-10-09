20c600b7d9a5e7fb6d952daea0dc040e918dc235
Max Ferreira <maxexatas@gmail.com>
2021-10-09 15:57:03 -0300

    Remove comentarios

--- src/main/java/com/joaotech/chatservice/service/AppConfig.java
+++ src/main/java/com/joaotech/chatservice/service/AppConfig.java
@@ -6,17 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.net.ssl.SSLContext;
 import java.io.File;
 import java.security.NoSuchAlgorithmException;
 
 @Configuration
 public class AppConfig {
-// TODO - Corrigir os getEnvs
-//private final String username = "keysPaces-dev-at-209085978178";
-//    private final String password = "C2tp/P3boE3Tz142ZUmVs4laC4KDHHPXRYeOwDgjWpg=";
-    private final String username = "";
-    private final String password = "";
     File driverConfig = new File(System.getProperty("user.dir") + "/src/main/resources/application.conf");
 
     @Primary
@@ -24,7 +18,6 @@ public class AppConfig {
     CqlSession session() throws NoSuchAlgorithmException {
         return CqlSession.builder().
                 withConfigLoader(DriverConfigLoader.fromFile(driverConfig)).
-                withAuthCredentials(username, password).
                 withKeyspace("chat_service_model").
                 build();
     }
