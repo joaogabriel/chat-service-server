3b113869203f338494f76151e8dc968c9c2a7e00
Max Ferreira <maxexatas@gmail.com>
2021-10-10 15:09:41 -0300

    Remove header e linhas desnecessárias

--- src/main/java/com/joaotech/chatservice/interceptor/ACKCommandChannelInterceptor.java
+++ src/main/java/com/joaotech/chatservice/interceptor/ACKCommandChannelInterceptor.java
@@ -3,7 +3,6 @@ package com.joaotech.chatservice.interceptor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -12,8 +11,6 @@ public class ACKCommandChannelInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-
-        MessageHeaders headers = message.getHeaders();
 
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
@@ -26,9 +23,6 @@ public class ACKCommandChannelInterceptor implements ChannelInterceptor {
         }
 
         String messageId = accessor.getMessageId();
-        String messageString = accessor.getMessage();
-
-        // TODO: 25/09/21 update status message
 
         return message;
 
