52d6d7f4268eda76e01c74f792941eb3428f1e0b
Max Ferreira <maxexatas@gmail.com>
2021-11-06 12:04:15 -0300

    Atualiza script de teste de performance e permite chamadas do front em localhost

--- src/main/java/com/joaotech/chatservice/configuration/SecurityConfig.java
+++ src/main/java/com/joaotech/chatservice/configuration/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
         config.setAllowCredentials(true);
-//        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
+//        config.setAllowedOriginPatterns(Arrays.asList("http://chat.testes.bringu.agsoft.com.br"));
+        config.addAllowedOriginPattern("http://chat.testes.bringu.agsoft.com.br");
         config.addAllowedOriginPattern("http://localhost:3000");
     //    config.addAllowedHeader("*");
         config.addAllowedMethod(HttpMethod.OPTIONS);
